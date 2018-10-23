032301694c5db3347dd917f4e2144e62338db80d
0154155lizhijian <1367688918@qq.com>
2018-10-23 19:04:37 +0800

    未完成

--- todolist/pages/index/index.js
+++ todolist/pages/index/index.js
@@ -5,27 +5,30 @@ const app = getApp()
 Page({
   data: {
     userInfo: {},
+    
     hasUserInfo: false,
-    status:1,  //任务状态 1全部 2未完成 3已完成
+    addShow: false,
+    addText: '',
+    status: 1,  //任务状态 1全部 2未完成 3已完成
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    lists:[{
-      text:'周末看电影',
-      status:0
-    },{
-      text:'考驾照',
-      status:0
-    },{
-      text:'学习MVVM',
-      status:1
-    },{
-      text:'跑步五公里',
-      status:0
+    lists: [{
+      text: '周末看电影',
+      status: 0
+    }, {
+      text: '考驾照',
+      status: 0
+    }, {
+      text: '学习MVVM',
+      status: 1
+    }, {
+      text: '跑步五公里',
+      status: 0
     }
-  ],
-    curLists:[]
+    ],
+    curLists: []
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -37,7 +40,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -58,14 +61,14 @@ Page({
         }
       })
     }
-    
-    this.setData({
-      curLists : this.data.lists
-    }); 
 
-    
+    this.setData({
+      curLists: this.data.lists
+    });
+
+
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     // this.data.userInfo = e.detail.userInfo;
@@ -74,27 +77,117 @@ Page({
       hasUserInfo: true
     })
   },
-  showStatus:function(e){
-    let status =  e.currentTarget.dataset.status
-
-    let lists = this.data.lists;
-    lists = lists.filter((item) => {
-      if (status == 1 ) return true;
-      if (status == 2 && item.status == 0) return true;
-      if (status == 3 && item.status == 1)
-      return true 
-    });
-    console.log(lists);
-
-    if (status == this.data.status) return;
+  showStatus: function (e) {
+    var st = e.currentTarget.dataset.status
+    if (this.data.status === st) return
+    if (st === '1') {
+      this.setData({
+        status: st,
+        curLists: this.data.lists
+      })
+      return
+    }
     this.setData({
-      status: status,
-      curLists: lists
+      status: st,
+      curLists: this.data.lists.filter(item => +item.status === (st - 2))
     })
-
-
   },
- 
+  changeTodo: function (e) {
+    var _this = this
+    var item = e.currentTarget.dataset.item
+    var temp = _this.data.lists
+    temp.forEach(el => {
+      if (el.id === item) {
+        if (el.status === '0') {
+          el.status = '1'
+          _this.showCur(temp)
+          wx.setStorage({
+            key: "lists",
+            data: temp
+          })
+          wx.showToast({
+            title: '已完成任务',
+            icon: 'success',
+            duration: 1000
+          });
+        } else {
+          wx.showModal({
+            title: '',
+            content: '该任务已完成，确定重新开始任务？',
+            confirmText: "确定",
+            cancelText: "不了",
+            success: function (res) {
+              if (res.confirm) {
+                el.status = '0'
+                _this.showCur(temp)
+                wx.setStorage({
+                  key: "lists",
+                  data: temp
+                })
+              } else {
+                return console.log('不操作')
+              }
+            }
+          })
+        }
+      }
+    })
+    console.log(item)
+  },
+  addTodoShow: function () {
+    this.setData({
+      addShow: true,
+      focus: true
+    })
+  },
+  addTodoHide: function () {
+    this.setData({
+      addShow: false,
+      focus: false,
+      addText: ''
+    })
+  },
+  setInput: function (e) {
+    this.setData({
+      addText: e.detail.value
+    })
+  },
+  addTodo: function () {
+    if (!this.data.addText.trim()) {
+      return
+    }
+    var temp = this.data.lists
+    var addT = {
+      id: new Date().getTime(),
+      title: this.data.addText,
+      status: '0'
+    }
+    temp.push(addT)
+    this.showCur(temp)
+    this.addTodoHide()
+    wx.setStorage({
+      key: "lists",
+      data: temp
+    })
+    wx.showToast({
+      title: '添加成功!',
+      icon: 'success',
+      duration: 1000
+    });
+  },
+  showCur: function (data) {
+    if (this.data.status === '1') {
+      this.setData({
+        lists: data,
+        curLists: data
+      })
+    } else {
+      this.setData({
+        lists: data,
+        curLists: data.filter(item => +item.status === (this.data.status - 2))
+      })
+    }
+  },
 
 
 
