615b18bd5e80f9f8bc3263029bc65220933a558e
0154155lizhijian <1367688918@qq.com>
2018-10-23 19:03:33 +0800

    从导航页到详情页

--- WXapp/pages/stories/index.js
+++ WXapp/pages/stories/index.js
@@ -1,6 +1,32 @@
 const app = getApp()
 
 Page({
-    data:{}
+  data: {
+    stories: [],
+    currentVid: null,
+    currentVideo: null
+  },
+  // 生命周期
+  onLoad(options) {
+    this.setData({
+      stories: app.globalData.stories
+    })
+  },
+  play(event) {
+    if(this.data.currentVid !== null){
+        this.data.currentVideo.pause();
+    }
+    const vid = event.currentTarget.dataset.vid
+    // console.log(vid);
+    const currentVideo = wx.createVideoContext(`${vid}`)
+    currentVideo.play()
 
+    this.setData({
+        currentVideo,
+        currentVid: vid
+      }, () => {
+        currentVideo.play()
+      })
+  
+  }
 });
