23ff0cc0403639a8cacd09c0fdff20abf5e10897
0154155lizhijian <1367688918@qq.com>
2018-11-05 14:51:47 +0800

    第三次提交

--- wx/second_hand_car/miniprogram/pages/index/index.js
+++ wx/second_hand_car/miniprogram/pages/index/index.js
@@ -12,7 +12,41 @@ Page({
       {type:'5万以下',
        hasIcon:false,
        icon:''
-    },'5-10万','10-15万','15万以上','大众','日产','现代','吉利','别克','雪佛兰','丰田','更多'],
+    },
+    {type:'5-10万',
+    hasIcon:false,
+    icon:''
+    }, {type:'10-15万',
+    hasIcon:false,
+    icon:''
+    },{type:'15万以上',
+    hasIcon:false,
+    icon:''
+    },{type:'大众',
+    hasIcon:true,
+    icon:'../../images/dazhong.png'
+    },{type:'日产',
+    hasIcon:true,
+    icon:'../../images/richan.png'
+    },{type:'现代',
+    hasIcon:true,
+    icon:'../../images/xiandai.png'
+    },{type:'吉利',
+    hasIcon:true,
+    icon:'../../images/jili.png'
+    },{type:'别克',
+    hasIcon:true,
+    icon:'../../images/bieke.jpg'
+    },{type:'雪佛兰',
+    hasIcon:true,
+    icon:'../../images/xuefulan.png'
+    },{type:'丰田',
+    hasIcon:true,
+    icon:'../../images/fengtian.png'
+    },{type:'更多',
+    hasIcon:true,
+    icon:'../../images/more.png'
+    },],
 
     navigation_bar_lists:
       [
@@ -20,42 +54,42 @@ Page({
           category:'二手车',
           icon_type :'che',
           color:'#0DC62A',
-          fontsize:'60rpx',
+          fontsize:'40rpx',
         },{
           category:'严选车',
           icon_type :'mai',
           color:'#6671ED',
-          fontsize:'80rpx',
+          fontsize:'60rpx',
         },{
           category:'免费卖车',
           icon_type:'qiandai',
           color:'#13DDB2',
-          fontsize:'60rpx',
+          fontsize:'40rpx',
         },{
           category:"快速卖车",
           icon_type:"mai1",
           color:'#FFA441',
-          fontsize:'46rpx',
+          fontsize:'30rpx',
         },{
           category:"估价",
           icon_type:"maichegujiapsd01",
           color:'#F75B5B',
-          fontsize:'60rpx',
+          fontsize:'40rpx',
         },{
           category:"查成交",
           icon_type:"chengjiao",
           color:'#2A92ED',
-          fontsize:'60rpx',
+          fontsize:'40rpx',
         },{
           category:"准新车",
           icon_type:"haopingwuxing",
           color:'#F65655',
-          fontsize:'60rpx',
+          fontsize:'40rpx',
         },{
           category:"降价急售",
           icon_type:"shandian",
           color:'#FFCF43',
-          fontsize:'60rpx',
+          fontsize:'40rpx',
         }
       ]
       ,[
@@ -63,22 +97,22 @@ Page({
           category:"今日超值",
           icon_type:"fire",
           color:'#FC781B',
-          fontsize:'60rpx'
+          fontsize:'40rpx'
         },{
           category:"练手车",
           icon_type:"quxuechelian",
           color:'#0CC129',
-          fontsize:'50rpx'
+          fontsize:'38rpx'
         },{
           category:"热销排行",
           icon_type:"guest-branch",
           color:'#FFA23E',
-          fontsize:'60rpx' 
+          fontsize:'40rpx' 
         },{
           category:"百科",
           icon_type:"baike-tianchong",
           color:'#3194EF',
-          fontsize:'60rpx'
+          fontsize:'40rpx'
         }
       ]
     ]
