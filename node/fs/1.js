48d823b7e54a45a7146473ac903817c36c670e9f
0154155lizhijian <1367688918@qq.com>
2018-11-10 10:20:25 +0800

    node处理文件读取的异步问以及readdirSync同步阻塞文件遍历查找

--- node/fs/1.js
+++ node/fs/1.js
@@ -1,11 +1,66 @@
-var fs = require('fs');
-var data;
-
+const fs = require('fs');
+// try {
+//     const dataA = fs.readFileSync('./a.txt','utf8',function(err,data){
+//     });
+//     const dataB = fs.readFileSync('./b.txt','utf8',function(err,data){
+//     });
+//     const dataC = fs.readFileSync('./C.txt','utf8',function(err,data){
+//     });
+//     console.log(dataA,dataB,dataC)
+// } catch (err) {
+//     console.error('文件读取错误:'+err.message);
+// }
 
 try {
-    data = fs.readFileSync('./fileForRead.txt','utf8');
+    const fileAPromise = new Promise((resolve,reject)=>{
+       fs.readFile('./a.txt','utf8',function(err,data){
+            if(err){
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        })
+    });
+    const fileBPromise = new Promise((resolve,reject)=>{
+        fs.readFile('./b.txt','utf8',function(err,data){
+             if(err){
+                 reject(err)
+             }else{
+                 resolve(data)
+             }
+         })
+     });
+     const fileCPromise = new Promise((resolve,reject)=>{
+        fs.readFile('./c.txt','utf8',function(err,data){
+             if(err){
+                 reject(err)
+             }else{
+                 resolve(data)
+             }
+         })
+     });
+     Promise.all([fileAPromise,fileBPromise,fileCPromise]).then(function(data){
+        console.log(data)
+     })
+    
+    // fileAPromise 
+    //     .then(data => {
+    //         console.log('A'+data);
+    //         return fileBPromise;    
+    //     })
+    //     .then(data => {  
+    //         console.log('B'+data)
+    //         return fileCPromise;
+    //     })
+    //     .then(data=>{
+    //         console.log('c'+data);
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //     })
 
-console.log(data);
-} catch (err) {
+
+   
+} catch (error) {
     console.error('文件读取错误:'+err.message);
 }
---
+++ node/fs/src/app.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>app</div>
+</template>
+<script>
+export default class app extends
+wepy.Page {
+    
+}
+</script>
+
+<style lang="stylus">
+
+</style>
