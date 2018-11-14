86850333ab88ecb96ccf2509973e247b9a12315c
0154155lizhijian <1367688918@qq.com>
2018-11-14 23:11:10 +0800

    typescript实现位图的布置以及工作流的初体验

---
+++ typescript/plane/webpack_plane/src/main.ts
@@ -0,0 +1,37 @@
+import { loadImage } from "./assetUtil";
+// egret Main 类 
+class Main extends egret.DisplayObjectContainer {
+  constructor () {
+    super(); 
+    // 如果子类要有自己的构造函数代码， 选运行父类构造函数
+    this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+  }
+  onAddToStage (event) {
+    // let textField = new egret.TextField();
+    // this.addChild(textField);
+    // textField.textColor = 0xff0000;
+    // textField.text = "您好";
+    this.createGame();
+  }
+  async createGame() {
+    // 加载资源 并行 Promise.All
+    const assets = [
+      'assets/background.png',
+      'assets/hero.png',
+      'assets/enemy.png'
+    ];
+    // loadImage Promise 
+    // assets => Promise[]
+    const bitmaps = await loadImage(assets) as egret.Bitmap[];
+    const [bg, hero, enemy] = bitmaps;
+    this.addChild(bg);
+    this.addChild(hero);
+    this.addChild(enemy);
+
+    enemy.x = this.stage.stageWidth/2;
+    
+  }
+}
+
+window['Main'] = Main;
+egret.runEgret();
