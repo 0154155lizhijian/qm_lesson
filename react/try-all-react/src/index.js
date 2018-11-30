120c35b35cb513ad964ba84746cb47d8b1fd9970
0154155lizhijian <1367688918@qq.com>
2018-11-30 15:06:15 +0800

    react输入框数据双向绑定

---
+++ react/try-all-react/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+class HelloUser extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            username:'wumian',
+            age:21
+        }
+        // this.handleChange = this.handleChange.bind(this);
+        // setTimeout(() => {
+        //     this.setState({
+        //         username:'chenyuhan'
+        //     })
+        // }, 2000);
+    }
+    render(){
+        return(
+            <div>Hello {this.state.username} i am {this.state.age} years old<nav></nav>
+            <input type="text" value={this.state.username} placeholder="小帅哥小美女快来玩啊" onChange={(e)=>{this.handleChange(e)}} />
+            </div>
+            
+        )
+        // return React.createElement(      //这是js的原生写法，很繁琐，无限递归
+        //     'div',
+        //     null,
+        //     React.createElement(
+        //         'h1',
+        //         null,
+        //         'hello,wumian!'
+        //     )
+        // )
+        
+    }
+    
+    handleChange(e){ 
+        this.setState({
+            username:e.target.value
+        })
+    }
+}
+ReactDOM.render(<HelloUser/>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
